e6f01e21be7bd047c546cf044d0dea4cd1d3a298
sky line <32512284+linesky@users.noreply.github.com>
2024-02-11 22:00:36 +0000

    Add files via upload

--- htmls.py
+++ htmls.py
@@ -1,4 +1,7 @@
 a:int=0
+styles=True
+scripts=True
+triggers=False
 filename="my.html"
 print("\x1bc\x1b[41;30m")
 f1=open(filename,"r")
@@ -10,21 +13,44 @@ for n in splits:
     n=n.replace("\n","")
     n=n.replace("\r","")
     tags=n.split("<")
-
+    triggers=False
     counter=0
     if len(tags)==2 and len(tags[0])>0:
-        print("\t",end="")
+        triggers=True
+        
     for ta in tags:
         ta=ta.strip()
         if counter==0:
             if len(ta)>0:
-               print(ta)
+               if ta=="style" or ta=="STYLE":
+                   styles=False
+               if ta=="script" or ta=="SCRIPT":
+                   scripts=False
+               if styles and scripts:
+                   if triggers:
+                       print("\t",end="")
+                   print(ta)
+               if ta=="/style" or ta=="/STYLE":
+                   styles=True
+               if ta=="/script" or ta=="/SCRIPT":
+                   scripts=True
         else:
             varsn=ta.split(" ")
             for varn in varsn:
                 varn=varn.strip()
                 if len(varn)>0:
-                    print(varn)
+                    if ta=="style" or ta=="STYLE":
+                        styles=False
+                    if ta=="script" or ta=="SCRIPT":
+                        scripts=False
+                    if styles and scripts: 
+                        print(varn)
+                    if ta=="/style" or ta=="/STYLE":
+                        styles=True
+                    if ta=="/script" or ta=="/SCRIPT":
+                        scripts=True
+                   
+
         
         counter+=1
         
